78666eb846cbecb3a0dbf0f0505cb09843a08cd7
carp51 <kitami2250@gmail.com>
2024-12-07 22:51:44 +0900

    first commit

--- C#/Exercise501/Program.cs
+++ C#/Exercise501/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Sample101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i + 1;
+            }
+
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
